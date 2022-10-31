a3100045c4753b2d7b2fc02dcbfb0636580b6cd1
Rambabu <rambabu@example.com>
2022-10-31 09:47:50 +0000

    Congrates Card

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Congratulation</h1>
+    <div className="card-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <p className="heading1">Kiran V</p>
+      <P className="paragaraph">
+        vishnu Institute of computer Education abd technology Bhimavaram
+      </P>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
